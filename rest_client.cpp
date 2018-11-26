8de2f17fc098c2f2ce3f98a6582db9b1193be2b0
Chris Warner <cdw.lighting@gmail.com>
2018-11-26 11:18:04 -0500

    Fix bug related to client not becoming available

--- rest_client.cpp
+++ rest_client.cpp
@@ -6,46 +6,48 @@ static const uint16_t TIMEOUT = 50000;
 
 RestClient::RestClient()
 {
-
 }
 
 RestClient::~RestClient()
 {
-
 }
 
-void RestClient::sendHeader(const char* aHeaderName, const char* aHeaderValue)
+void RestClient::sendHeader(const char *aHeaderName, const char *aHeaderValue)
 {
     _client.print(aHeaderName);
     _client.print(": ");
     _client.println(aHeaderValue);
 }
 
-void RestClient::sendHeader(const char* aHeaderName, const int aHeaderValue)
+void RestClient::sendHeader(const char *aHeaderName, const int aHeaderValue)
 {
     _client.print(aHeaderName);
     _client.print(": ");
     _client.println(aHeaderValue);
 }
 
-void RestClient::sendHeader(const char* aHeaderName)
+void RestClient::sendHeader(const char *aHeaderName)
 {
     _client.println(aHeaderName);
 }
 
 void RestClient::request(http_request_t &aRequest, http_response_t &aResponse,
-    http_header_t headers[], const char* aHttpMethod)
+                         http_header_t headers[], const char *aHttpMethod)
 {
     aResponse.status = -1;
 
     bool connected = false;
-    if(aRequest.hostname !=NULL) {
+    if (aRequest.hostname != NULL)
+    {
         connected = _client.connect(aRequest.hostname.c_str(), (aRequest.port) ? aRequest.port : 80);
-    } else {
+    }
+    else
+    {
         connected = _client.connect(aRequest.ip, aRequest.port);
     }
 
-    if(!connected) {
+    if (!connected)
+    {
         _client.stop();
 
         return;
@@ -56,16 +58,18 @@ void RestClient::request(http_request_t &aRequest, http_response_t &aResponse,
     _client.print(aRequest.path);
     _client.print(" HTTP/1.1\r\n");
 
-    sendHeader("Connection", "close");
-    if(aRequest.hostname != NULL)
+    // sendHeader("Connection", "close");
+    if (aRequest.hostname != NULL)
     {
         sendHeader("HOST", aRequest.hostname.c_str());
     }
 
-    if(aRequest.body != NULL)
+    if (aRequest.body != NULL)
     {
         sendHeader("Content-Length", (aRequest.body).length());
-    } else if (strcmp(aHttpMethod, HTTP_METHOD_POST) == 0) {
+    }
+    else if (strcmp(aHttpMethod, HTTP_METHOD_POST) == 0)
+    {
         sendHeader("Content-Length", 0);
     }
     sendHeader("Cache-Control", "no-cache");
@@ -74,7 +78,7 @@ void RestClient::request(http_request_t &aRequest, http_response_t &aResponse,
 
     sendHeader("User-Agent", "ES8266");
 
-    if(strcmp(aHttpMethod, HTTP_METHOD_POST) == 0)
+    if (strcmp(aHttpMethod, HTTP_METHOD_POST) == 0)
     {
         sendHeader("Content-Type", "application/x-www-form-urlencoded");
     }
@@ -84,9 +88,12 @@ void RestClient::request(http_request_t &aRequest, http_response_t &aResponse,
         int i = 0;
         while (headers[i].header != NULL)
         {
-            if(headers[i].value != NULL) {
+            if (headers[i].value != NULL)
+            {
                 sendHeader(headers[i].header, headers[i].value);
-            } else {
+            }
+            else
+            {
                 sendHeader(headers[i].header);
             }
             i++;
@@ -98,7 +105,7 @@ void RestClient::request(http_request_t &aRequest, http_response_t &aResponse,
     _client.println();
     _client.flush();
 
-    if(aRequest.body != NULL)
+    if (aRequest.body != NULL)
     {
         _client.println(aRequest.body);
     }
@@ -111,54 +118,66 @@ void RestClient::request(http_request_t &aRequest, http_response_t &aResponse,
     bool error = false;
     bool timeout = false;
 
-    do {
-        while (_client.available()) {
+    delay(400);
+    do
+    {
+        Serial.print("Client is available: ");
+        String isAvailable = _client.available() ? "Yes" : "No";
+        Serial.println(isAvailable);
+
+        while (_client.available())
+        {
             char c = _client.read();
             lastRead = millis();
 
-            if (c == -1) {
+            if (c == -1)
+            {
                 error = true;
                 break;
             }
 
-            if(bufferPosition < sizeof(buffer)-1) {
+            if (bufferPosition < sizeof(buffer) - 1)
+            {
                 buffer[bufferPosition] = c;
-            } else if ((bufferPosition == sizeof(buffer)-1)) {
+            }
+            else if ((bufferPosition == sizeof(buffer) - 1))
+            {
                 buffer[bufferPosition] = '\0';
                 _client.stop();
-                error=true;
-
+                error = true;
             }
             bufferPosition++;
         }
         buffer[bufferPosition] = '\0';
-
+        Serial.println("Finished Received Data");
         timeout = millis() - lastRead > TIMEOUT;
 
-        if(!error && !timeout) {
+        if (!error && !timeout)
+        {
             delay(200);
         }
-    }while(_client.connected() && !timeout && !error);
+    } while (_client.connected() && !timeout && !error);
 
     _client.stop();
 
-    if(timeout) {
-      Serial.println("Timed Out!");
+    if (timeout)
+    {
+        Serial.println("Timed Out!");
         return;
     }
 
     String raw_response(buffer);
 
-    String statusCode = raw_response.substring(9,12);
+    String statusCode = raw_response.substring(9, 12);
 
     int bodyPos = raw_response.indexOf("\r\n\r\n");
-    if (bodyPos == -1) {
-      Serial.println("Invalid Body.");
+    if (bodyPos == -1)
+    {
+        Serial.println("Invalid Body.");
         return;
     }
 
     aResponse.body = "";
     aResponse.body += raw_response.substring(bodyPos + 4);
     aResponse.status = atoi(statusCode.c_str());
-
 }
